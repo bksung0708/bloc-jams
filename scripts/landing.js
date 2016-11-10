cd6775ed932b4bb4a8d190311a107faba7feae01
Brian Sung <bksung0708@gmail.com>
2016-11-10 12:05:48 -0800

    assignment 8 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,14 @@
  var animatePoints = function() {
  
      var points = document.getElementsByClassName('point');
- 
+     
+     var revealPoint = function(index) {
+         points[index].style.opacity = 1;
+         points[index].style.transform = "scaleX(1) translateY(0)";
+         points[index].style.msTransform = "scaleX(1) translateY(0)";
+         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+     }
+     
      var revealFirstPoint = function() {
          points[0].style.opacity = 1;
          points[0].style.transform = "scaleX(1) translateY(0)";
@@ -23,8 +30,8 @@
          points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
      };
  
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
+     for(var i = 0; i < points.length; i++) {
+         revealPoint(i);
+     }
  
  };
